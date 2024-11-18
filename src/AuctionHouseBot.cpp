40ed144c71ae28471ce3e6eec03121e3452b83d1
avirar <luke.thorpe@gmail.com>
2024-11-18 16:44:31 +1100

    Update AuctionHouseBot.cpp

--- src/AuctionHouseBot.cpp
+++ src/AuctionHouseBot.cpp
@@ -17,6 +17,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
+#include <ctime>
+#include <sstream>
 #include "ObjectMgr.h"
 #include "AuctionHouseMgr.h"
 #include "Config.h"
